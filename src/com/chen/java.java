8835c1ab53842bf85da5e0d0dd2b45fe54e6e79d
chenfeiyang1990 <954551546>
2015-04-08 09:40:42 +0800

    first.txt

---
+++ src/com/chen/java.java
@@ -0,0 +1,10 @@
+package com.chen;
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.printf("Hello World!");
+	}
+
+}
---
+++ 数列/src/shuzu/shuzu.java
@@ -0,0 +1,30 @@
+package shuzu;
+
+public class shuzu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0,j=0;
+		int n=x.nextInt();
+		int a[n][n];
+		
+		
+     for(i=0;i<10;i++)
+     {
+    	a[i][0]=1;
+    	a[i][i]=1;
+     } 
+     for(i=2;i<10;i++)
+     for(j=1;j<i;j++)
+     {
+    	a[i][j]=a[i-1][j]+a[i-1][j-1];
+     }
+     
+    	 
+	 for(i=10;i>=0;i--)
+   	 for(j=0;j<=i;j++)
+   	 {
+   		 System.out.printf("%5d",a[i][j]);
+	}
+	}
+}
